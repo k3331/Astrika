a497d4eb073430378b46af9833c26218eb369634
Aditya <aditya.tripathi@onicares.com>
2022-09-29 18:08:53 +0530

    changes in delete reducer

--- src/redux/Reducers.js
+++ src/redux/Reducers.js
@@ -6,14 +6,13 @@
  
  const Reducer =(state=initState,action) =>{
   if(action.type ==='DELETE_POST'){
-     let newProj = state.posts.filter((pro) => {return pro.id !== action.id})
+     let newPost = state.posts.filter((pro) => {return pro.id !== action.id})
      return{
       ...state,
-      projects:newProj
+      posts:newPost
      }
   }
    if(action.type ==='ADD'){
-    console.log('reducer is called',action.payload)
     return{
 
       ...state,
